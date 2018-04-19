f0f893af02bbf1fb732b9e71239e2efbec53be91
Glyn Hudson <glyn.hudson@gmail.com>
2018-04-19 11:38:49 +0100

    add openevse

--- emonupload.py
+++ emonupload.py
@@ -16,7 +16,7 @@ from os.path import expanduser
 #--------------------------------------------------------------------------------------------------
 DEBUG             = 0
 UPDATE            = 1            # Update firmware releases at startup
-VERSION = 'V1.8.0'
+VERSION = 'V1.9.0'
 
 download_folder = 'latest/'
 repo_folder = 'repos/'
@@ -99,24 +99,23 @@ def interent_connected(url):
 # Update emonupload
 #--------------------------------------------------------------------------------------------------
 def update_emonupload(filename):
-    # if (DEBUG): print 'Checking for emonUpload updates...'
-    # dir_path=os.path.dirname(os.path.realpath(filename))
-    # if (DEBUG): print 'git abs path' + dir_path
-    # g = git.cmd.Git(dir_path)
-    # r = g.pull()
-    # if (DEBUG): print g
-    # if r != 'Already up-to-date.':
-    #     print r
-    #     print bcolors.WARNING + 'UPDATE FOUND....emonUpload restart required\nEXITING...' + bcolors.ENDC
-    #     os.system("rm -rf " + download_folder)
-    #     os.system("rm -rf " + repo_folder)
-    #     if (DEBUG): raw_input("\nPress Enter to continue...\n")
-    #     quit()
-    # else:
-    #     print bcolors.OKGREEN + 'Already up-to-date: emonUpload' + bcolors.ENDC
-    #     if (DEBUG): raw_input("\nPress Enter to continue...\n")
-    # return r
-    return
+    if (DEBUG): print 'Checking for emonUpload updates...'
+    dir_path=os.path.dirname(os.path.realpath(filename))
+    if (DEBUG): print 'git abs path' + dir_path
+    g = git.cmd.Git(dir_path)
+    r = g.pull()
+    if (DEBUG): print g
+    if r != 'Already up-to-date.':
+        print r
+        print bcolors.WARNING + 'UPDATE FOUND....emonUpload restart required\nEXITING...' + bcolors.ENDC
+        os.system("rm -rf " + download_folder)
+        os.system("rm -rf " + repo_folder)
+        if (DEBUG): raw_input("\nPress Enter to continue...\n")
+        quit()
+    else:
+        print bcolors.OKGREEN + 'Already up-to-date: emonUpload' + bcolors.ENDC
+        if (DEBUG): raw_input("\nPress Enter to continue...\n")
+    return r
 #--------------------------------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------------------------------
@@ -470,15 +469,18 @@ while(1):
     print bcolors.OKGREEN + '(x) emonTx V3\n' + bcolors.ENDC
     print bcolors.OKGREEN + '(i) emonPi\n' + bcolors.ENDC
     print bcolors.OKGREEN + '(h) emonTH V2\n' + bcolors.ENDC
+    print '\n'
+    print bcolors.OKGREEN + '(w) IoTaWatt' + bcolors.ENDC
+    print bcolors.OKGREEN + '(e) emonESP' + bcolors.ENDC
+    print bcolors.OKGREEN + '(3) 3-phase emonTx' + bcolors.ENDC
+    print bcolors.OKGREEN + '(r) MQTT WiFi Relay' + bcolors.ENDC
+
 
     print '\n'
-    print bcolors.OKGREEN + '(3) 3-phase emonTx' + bcolors.ENDC
-    print bcolors.OKGREEN + '(e) emonESP' + bcolors.ENDC
-    print bcolors.OKGREEN + '(w) IoTaWatt' + bcolors.ENDC
     # print '\n'
-    print bcolors.OKGREEN + '(v) WiFi OpenEVSE/EmonEVSE' + bcolors.ENDC
-    print bcolors.OKGREEN + '(z) Controller EmonEVSE' + bcolors.ENDC
-    print bcolors.OKGREEN + '(r) MQTT WiFi Relay' + bcolors.ENDC
+    print bcolors.OKGREEN + '(z) Controller EmonEVSE (ISP)' + bcolors.ENDC
+    print bcolors.OKGREEN + '(v) WiFi ESP OpenEVSE/EmonEVSE' + bcolors.ENDC
+    print bcolors.OKGREEN + '(p) Controller OpenEVSE (ISP)' + bcolors.ENDC
 
     print '\n'
     #print bcolors.OKGREEN + '(r) for RFM69Pi' + bcolors.ENDC
@@ -567,7 +569,7 @@ while(1):
         cmd = 'pip freeze --disable-pip-version-check | grep esptool'
         if subprocess.call(cmd, shell=True) != ' ':
             # If esptool is installed
-            cmd = 'esptool.py write_flash 0x000000 ' + download_folder + 'openenergymonitor-emonesp.bin' + ' 0x300000 ' +    download_folder + 'openenergymonitor-emonesp-spiffs.bin'
+            cmd = 'esptool.py write_flash 0x000000 ' + download_folder + 'openenergymonitor-emonesp-firmware.bin' + ' 0x300000 ' +    download_folder + 'openenergymonitor-emonesp-spiffs.bin'
             print cmd
             subprocess.call(cmd, shell=True)
             if raw_input("\nDone emonESP upload. Press Enter to return to menu or (s) to view serial output (reset required)>\n"):
@@ -609,9 +611,9 @@ while(1):
         os.system('clear') # clear terminal screen Linux specific
         
 
-    # EmonESP controller
+    # EmonEVSE controller
     elif nb=='z':
-        print bcolors.OKGREEN + '\nOpenEVSE WiFi Upload\n' + bcolors.ENDC
+        print bcolors.OKGREEN + '\nEmonEVSE Controller Upload (via ISP)\n' + bcolors.ENDC
         cmd = 'pip freeze --disable-pip-version-check | grep esptool'
         if subprocess.call(cmd, shell=True) != ' ':
             # If esptool is installed
@@ -619,6 +621,18 @@ while(1):
             print cmd
             subprocess.call(cmd, shell=True)
             raw_input("\nDone EmonEVSE controller upload. Press Enter to return to menu\n")
+        os.system('clear') # clear terminal screen Linux specific
+        
+    # OpenEVSE controller
+    elif nb=='p':
+        print bcolors.OKGREEN + '\nOpenEVSE Controller Upload (via ISP)\n' + bcolors.ENDC
+        cmd = 'pip freeze --disable-pip-version-check | grep esptool'
+        if subprocess.call(cmd, shell=True) != ' ':
+            # If esptool is installed
+            cmd = 'avrdude -p atmega328p -c usbasp -P usb -e -U flash:w:' + download_folder + 'openenergymonitor-open_evse-openevse.hex'
+            print cmd
+            subprocess.call(cmd, shell=True)
+            raw_input("\nDone OpenEVSE controller upload. Press Enter to return to menu\n")
         os.system('clear') # clear terminal screen Linux specific
         
 
